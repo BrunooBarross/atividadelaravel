f4f408e8636d1d1e3b9bc69f6189b638e8a4ae8b
BrunooBarross <barrosbrunofelipe@gmail.com>
2021-10-19 13:30:34 -0300

    primeiro commit

---
+++ resources/views/site/visual/listar.blade.php
@@ -0,0 +1,62 @@
+{{-- Extender o layout principal --}}
+@extends('site.layouts.principal')
+
+{{-- Section para o titulo --}}
+@section('titulo','Cliente')
+
+{{-- Section para a acao --}}
+@section('acao', 'Listar')
+
+{{-- Section Principal --}}
+@section('conteudo-principal') {{-- Tem que colocar o nome do yeild onde quero jogar o conteudo  --}}
+
+
+<table class="table table-hover">
+    <div>
+        <table class="table table-dark">
+            <thead>
+              <tr>
+                <th scope="col">id</th>
+                <th scope="col">pedido</th>
+                <th scope="col">preço</th>                               
+              </tr>
+
+            </thead>
+            <tbody>
+                @forelse ($lista as $lista)
+                <tr>
+                  <td>{{$lista->id}}</td> 
+                  <td>{{$lista->pedido}}</td>
+                  <td>{{$lista->preco}}</td>                                     
+                </tr>            
+                @empty
+                  <tr>
+                    <td colspan="2">Não Existem Cadastros</td>
+                  </tr>
+                    
+                @endforelse($lista as $lista)
+                      
+            </tbody>
+            
+        </table>
+
+        @if (session('sucesso'))
+          <div>
+              {{session('sucesso')}}
+          </div>
+           
+       @endif
+
+        <div class="span3">
+
+        <a href="{{route('site.pedido')}}" class="btn btn-primary btn-lg btn-block">Add Pedido</a> <hr/>
+        <a href="{{route('site.cliente')}}" class="btn btn-primary btn-lg btn-block">Add Cliente</a> <hr/>
+        
+            
+       </div>       
+        
+    </div>
+    
+
+</table>
+@endsection
